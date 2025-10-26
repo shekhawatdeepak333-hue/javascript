83b5c0a328ff7e675a7f98b911c4a4e7ba4c826f
shekhawatdeepak333-hue <shekhawatdeepak333@gmail.com>
2025-10-26 20:59:22 +0000

    container config done

---
+++ 01_basics/01_let_const_var.js
@@ -0,0 +1,15 @@
+const user_email= "deepak@gmail.com"
+let password="1234"
+var state="haryana"
+language="hindi"
+console.log(user_email);
+//log se single single value print hogi jo ki boht time taking hai so we use table 
+console.table([user_email,password,state,language])
+/*
+consonant define karne ka bas 1 rasta hai = const
+but variable define karne ka 2 rasta hai = var and let
+and we will always use let
+*/
+user_email="shekhawat@gmail.com"
+password="5678"
+// here we can change the values but changes in constant values are not allowed and var changes
